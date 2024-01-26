18c95e781a97e54d6deeb8b81b5d990b5e170305
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-01-26 12:52:30 -0500

    Implemented Constructos for Date Module

--- Date.cpp
+++ Date.cpp
@@ -39,4 +39,36 @@ namespace sdds
         mon--;
         return days[mon] + int((mon == 1) * ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
     }
+
+    //Deault Constructor for date class
+    Date::Date()
+    {
+        getSystemDate(m_year, m_month, m_day, m_hour, m_min, false);
+        m_onlyDate = false;
+    }
+
+    //Three argument constructor for date class
+    Date::Date(int year, int month, int day)
+    {
+        m_year = year;
+        m_month = month;
+        m_day = day;
+        m_hour = m_min = 0;
+        m_onlyDate = true;
+
+        validate(m_year, m_month, m_day, m_hour, m_min);
+    }
+
+    //Five argument constructor for date class
+    Date::Date(int year, int month, int day, int hour, int min)
+    {
+        m_year = year;
+        m_month = month;
+        m_day = day;
+        m_hour = hour;
+        m_min = min;
+        m_onlyDate = false;
+
+        validate(m_year, m_month, m_day, m_hour, m_min);
+    }
 }
