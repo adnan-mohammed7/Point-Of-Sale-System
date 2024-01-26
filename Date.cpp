0e06204bf0735ed954ea56ad6fd770cfc339553f
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-01-26 12:51:47 -0500

    Implemented uniqueDate and days calculation functionality in Date module

--- Date.cpp
+++ Date.cpp
@@ -26,4 +26,17 @@ namespace sdds
             min = lt.tm_min;
         }
     }
+
+    //To check for unique date
+    int uniqueDateValue(int year, int mon, int day, int hour, int min) {
+        return year * 535680 + mon * 44640 + day * 1440 + hour * 60 + min;
+    }
+
+    //To check for days in a month
+    int daysOfMonth(int year, int month) {
+        int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
+        int mon = month >= 1 && month <= 12 ? month : 13;
+        mon--;
+        return days[mon] + int((mon == 1) * ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+    }
 }
