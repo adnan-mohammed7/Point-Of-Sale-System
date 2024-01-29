a2feb948f321587ffff3b804f3275bc0c4686b68
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-01-29 10:46:08 -0500

    Added prototypes of PosIO module

--- PosIO.h
+++ PosIO.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef POSIO_H
+#define POSIO_H
+
+#include <iostream>
+#include <fstream>
+
+namespace sdds
+{
+	class PosIO {
+	public:
+		//Pure Virtual Functions
+		virtual std::ostream& write(std::ostream& os) const = 0;
+		virtual std::istream& read(std::istream& is) = 0;
+		virtual std::ofstream& save(std::ofstream& os) const = 0;
+		virtual std::ifstream& load(std::ifstream& is) = 0;
+		virtual ~PosIO();
+	};
+	//Iostream & Ifstream overload
+	std::ostream& operator<<(std::ostream& os, const PosIO& rhs);
+	std::ofstream& operator<<(std::ofstream& os, const PosIO& rhs);
+	std::istream& operator>>(std::istream& is, PosIO& rhs);
+	std::ifstream& operator>>(std::ifstream& is, PosIO& rhs);
+}
+#endif // !POSIO_H
