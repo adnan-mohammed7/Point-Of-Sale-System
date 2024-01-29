d41d1073807f6c051ac896a3dc2a4ce9a30df27d
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-01-29 11:11:32 -0500

    Implemented PosIO module

--- PosIO.cpp
+++ PosIO.cpp
@@ -9,5 +9,24 @@ namespace sdds
 	PosIO::~PosIO()
 	{
 	}
-
+	std::ostream& operator<<(std::ostream& os, const PosIO& rhs)
+	{
+		//Calling the defined function
+		return rhs.write(os);
+	}
+	std::ofstream& operator<<(std::ofstream& os, const PosIO& rhs)
+	{
+		//Calling the defined function
+		return rhs.save(os);
+	}
+	std::istream& operator>>(std::istream& is, PosIO& rhs)
+	{
+		//Calling the defined function
+		return rhs.read(is);
+	}
+	std::ifstream& operator>>(std::ifstream& is, PosIO& rhs)
+	{
+		//Calling the defined function
+		return rhs.load(is);
+	}
 }
