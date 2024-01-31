df39a4f7133a3f989c3ff7021624f7e32ff7033b
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-01-31 00:40:45 -0500

    Added funtion prototype to NonPerishable module

--- NonPerishable.h
+++ NonPerishable.h
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef NONPERISHABLE_H
+#define NONPERISHABLE_H
+
+#include "Item.h"
+
+namespace sdds
+{
+	class NonPerishable : public Item {
+	public:
+		//To return the type of item nonperishable
+		char itemType()const;
+
+		//To display nonperishable item to ostream
+		std::ostream& write(std::ostream& os) const;
+	};
+}
